97b0ed4a966aaf982d2c8d3086aae7003e8a2f47
denis <denis.lachartre@gmail.com>
2024-06-22 19:14:39 +0200

    viewport

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -1,5 +1,6 @@
 import type { World } from '@dimforge/rapier2d'
 import { Application, Graphics } from 'pixi.js'
+import { Viewport } from 'pixi-viewport'
 
 export default function () {
   const world = ref<World>()
@@ -20,27 +21,50 @@ export default function () {
         resizeTo: window,
       })
 
-      app.stage.eventMode = 'static'
-      app.stage.hitArea = app.screen
-      app.stage.on('pointerup', onDragEnd)
-      app.stage.on('pointerupoutside', onDragEnd)
+      // create viewport
+      const viewport = new Viewport({
+        screenWidth: window.innerWidth,
+        screenHeight: window.innerHeight,
+        worldWidth: 10000,
+        worldHeight: 10000,
+        events: app.renderer.events,
+      })
 
-      const balls = Array.from({ length: 500 }).map(() => {
+      // add the viewport to the stage
+      app.stage.addChild(viewport)
+
+      // activate plugins
+      viewport
+        .drag()
+        .pinch()
+        .wheel({
+          percent: 2,
+          smooth: 20,
+        })
+        .decelerate({
+          friction: 0.93,
+        })
+        .moveCenter(viewport.worldWidth / 2, viewport.worldHeight / 2)
+        .clamp({
+          direction: 'all',
+        })
+
+      const balls = Array.from({ length: 10 }).map(() => {
         if (!world.value)
           throw new Error('error')
 
         const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-          .setTranslation(window.innerWidth / 2 + Math.random(), window.innerHeight / 2 + Math.random())
+          .setTranslation(viewport.worldWidth / 2 + Math.random(), viewport.worldHeight / 2 + Math.random())
           .setLinearDamping(10)
         const rigidBody = world.value.createRigidBody(rigidBodyDesc)
 
-        const colliderDesc = RAPIER.ColliderDesc.ball(50)
+        const colliderDesc = RAPIER.ColliderDesc.ball(10)
         world.value?.createCollider(colliderDesc, rigidBody)
 
         const graphic = new Graphics()
-        graphic.circle(colliderDesc.translation.x, colliderDesc.translation.y, 48)
+        graphic.circle(colliderDesc.translation.x, colliderDesc.translation.y, 9.5)
         graphic.fill(0xDE3249)
-        app.stage.addChild(graphic)
+        viewport.addChild(graphic)
 
         graphic.eventMode = 'static'
         graphic.cursor = 'pointer'
@@ -49,42 +73,25 @@ export default function () {
           rigidBody,
           graphic,
         }
-        graphic.on('pointerdown', () => onDragStart(ball))
 
         return ball
       })
 
-      let dragTarget: typeof balls[number] | null
+      const graphic = new Graphics({
+        zIndex: -1,
+      })
+      graphic.rect(0, 0, viewport.worldWidth, viewport.worldHeight)
+      graphic.fill(0x101220)
+      viewport.addChild(graphic)
 
-      function onDragMove(event: any) {
-        if (dragTarget) {
-          dragTarget.graphic.parent.toLocal(event.global, undefined, dragTarget.graphic.position)
-          dragTarget.rigidBody.setTranslation(event.global, true)
-        }
-      }
-
-      function onDragStart(ball: typeof balls[number]) {
-        dragTarget = ball
-        app.stage.on('pointermove', onDragMove)
-      }
-
-      function onDragEnd() {
-        if (dragTarget) {
-          app.stage.off('pointermove', onDragMove)
-          dragTarget = null
-        }
-      }
-
-      function update() {
+      /**
+       * Add physics loop
+       */
+      app.ticker.add(() => {
         world.value?.step()
-
         for (const ball of balls)
           ball.graphic.position = ball.rigidBody.translation()
-
-        requestAnimationFrame(update)
-      }
-
-      requestAnimationFrame(update)
+      })
     })
   }
 
